388fa3be78ab42da8bade2388585517f13b6a0e5
cyril.chang <hsiao.jung@msn.com>
2020-06-14 15:51:12 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+
+// src/screens/Home.js
+
+import React from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+import MainStackNavigator from './components/MainStackNavigator'
+
+
+export default function App() {
+  return <MainStackNavigator />
+}
